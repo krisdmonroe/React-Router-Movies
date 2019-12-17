5b47b7226bfc83ada4f8f1f563450777d962847a
Kris Monroe <krisdmonroe@gmail.com>
2019-12-17 15:48:18 -0800

    mvp will attempt stretch after a break

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-
+import Movie from './Movies/Movie'
+import MovieList from './Movies/MovieList'
+import axios from 'axios';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+    <Route exact path='/'> 
+      <MovieList movies={movies}/> 
+    </Route>
+    <Route path='/movies/:id'> 
+      <Movie movies={movies}/> 
+    </Route>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,36 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState();
- 
-  useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-  },[]);
-  
+  const {id} = useParams()
+  const movieParam = props.movies.find(movie => id === `${movie.id}`)
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
 
-  if (!movie) {
+  if (!movieParam) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars } = movieParam;
   return (
     <div className="save-wrapper">
       <div className="movie-card">
